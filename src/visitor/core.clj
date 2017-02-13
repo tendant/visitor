41c3e00e8bcf157f92953265a1bcddb820e1124c
Lei Wang <tendant@gmail.com>
2017-02-13 15:44:46 -0800

    add visitor

--- src/visitor/core.clj
+++ src/visitor/core.clj
@@ -1,7 +1,30 @@
 (ns visitor.core
+  (:require [clojure.zip :as z]
+            [zip.visit :as zv])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn branch? [node]
+  (map? node))
+
+(defn children [node]
+  (:children node))
+
+(defn make-node [node children]
+  (assoc node :children children))
+
+(def test1 {:children []})
+
+(zv/defvisitor visited :pre [n s]
+  (println :pre "node:" n " state:" s)
+  {:node n
+   :state (assoc s :change "visited")})
+
+(defn test-visit
+  [root]
+  (let [zipper (z/zipper branch? children make-node root)]
+    (zv/visit zipper nil [visited])))
