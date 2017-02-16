e8057baf86e5fec7e63b8eb6d93fced64a823634
Lei Wang <tendant@gmail.com>
2017-02-16 09:03:35 -0800

    update test

--- src/visitor/core.clj
+++ src/visitor/core.clj
@@ -3,37 +3,34 @@
             [zip.visit :as zv])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
 (defn branch? [node]
   (println "branch?:" node)
   (map? node))
 
-(defn children [node]
-  (println "children:" node)
-  (:children node))
-
-(defn add-parent-to-children
+(defn add-parent-path-to-children
   [parent children]
   (map #(assoc % :v/parent parent) children))
 
+(defn children [node]
+  (println "children:" node)
+  (add-parent-path-to-children node (:children node)))
+
 (defn make-node [node children]
   (println "make-node:" node " with children:" children)
-  (assoc node :children (add-parent-to-children node children)))
-
-(def test1 {:name "first" :children [{:name "child1"
-                                      :children {:name "child1's children" :children [{:name "child1's children 1"} {:name "child1's chidlren 2"}]}}
-                                     {:name "child2"}]})
+  (if node
+    (assoc node :children children)))
 
 (zv/defvisitor visited :pre [n s]
   (println :pre "node:" n " state:" s)
   {:node n
-   :state (assoc s :parent n)})
+   :state s})
 
 (defn test-visit
   [root]
   (let [zipper (z/zipper branch? children make-node root)]
     (zv/visit zipper nil [visited])))
+
+(def test1 {:name "first" :children [{:name "child1"
+                                      :children [{:name "child1's children 1"} {:name "child1's chidlren 2"}]}
+                                     {:name "child2"}]})
+
